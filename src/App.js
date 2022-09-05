d4fff9d8b7e36064f370c52544a2fc982dac793d
Agnieszka Grosicka <a.drozdowska91@gmail.com>
2022-09-05 15:02:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Records from './smartphons.json';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Records.map(record=>{
+        return(
+          <div>
+            <img src={record.img}/>
+            {record.id}
+          </div>
+        )
+      })}
     </div>
   );
 }
